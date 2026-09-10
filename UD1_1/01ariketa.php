d8a60575fc1b6f337a025e802249b9bf03db877b
ego-ara <e_araukuavar@fpzornotzalh.eus>
2026-09-10 09:57:42 +0200

    UD1_1-01ariketa

---
+++ UD1_1/01ariketa.php
@@ -0,0 +1,10 @@
+<?php 
+$zenbaki1 = 2;
+$zenbaki2 = 2;
+echo("Batura: " .       $zenbaki1+$zenbaki2)."<br>";
+echo("Kenketa: " .      $zenbaki1-$zenbaki2)."<br>";
+echo("Biderketa: " .    $zenbaki1*$zenbaki2)."<br>";
+echo("Zatiketa: " .     $zenbaki1/$zenbaki2)."<br>";
+echo("Modulua: " .      $zenbaki1%$zenbaki2)."<br>";
+echo("Berreketa: " .    $zenbaki1**$zenbaki2)."<br>";
+?>
