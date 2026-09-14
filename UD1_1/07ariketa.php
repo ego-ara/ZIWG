dc506d42069004a882729ae30daca7492b1dd698
ego-ara <e_araukuavar@fpzornotzalh.eus>
2026-09-14 10:27:44 +0200

    UD1_1-07ariketa

---
+++ UD1_1/07ariketa.php
@@ -0,0 +1,18 @@
+<?php
+$x = 1;
+
+if($x>=0 && $x<=4.9){
+    echo("Ez gainditua");
+}elseif($x>=5 && $x<=5.9){
+    echo("Nahiko");
+}elseif($x>=6 && $x<=6.9){
+    echo("Ondo");
+}elseif($x>=7 && $x<=8.9){
+    echo("Oso ondo");
+}elseif($x>=9 && $x<=10){
+    echo("Bikain");
+}else{
+    echo("AKATSA");
+}
+
+?>
