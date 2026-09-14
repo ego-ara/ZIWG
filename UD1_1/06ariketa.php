15d02adcc535af7bebcaba63c05306a44c05d2e5
ego-ara <e_araukuavar@fpzornotzalh.eus>
2026-09-14 10:22:05 +0200

    UD1_1-06ariketa

---
+++ UD1_1/06ariketa.php
@@ -0,0 +1,15 @@
+<?php
+$x = 1;
+$y = 2;
+
+$batura = $x+$y;
+$bidekaketa = $x*$y;
+
+if($batua>$bidekaketa){
+    echo("Batura biderketa baino handiagoa da");
+}else{
+    echo("Biderketa gehiketa baino handiagoa da");
+}
+
+
+?>
